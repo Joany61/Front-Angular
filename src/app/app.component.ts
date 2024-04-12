9fb5d595467353ca0f0956342a390d7430f10cce
Joany <rafanomezantsoa617@gmail.com>
2024-04-12 17:38:30 +0300

    ajout branche master et commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpConnectionService } from './services/http-connection.service';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../_module/material.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, MaterialModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   providers: [HttpConnectionService],
